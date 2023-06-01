c380ac3f8721ae5e46c1ce3fcd0fe99a0fb04c8b
Ruslan Himadieiev <113462988+Himadieiev@users.noreply.github.com>
2023-06-01 15:37:41 +0300

    add

---
+++ src/redux/Filter/filterSlice.js
@@ -0,0 +1,16 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { filterInitialState } from './initialState';
+
+export const filterSlice = createSlice({
+  name: 'filter',
+  initialState: filterInitialState,
+  reducers: {
+    setFilter(state, action) {
+      state.value = action.payload;
+    },
+  },
+});
+
+export const filterReducer = filterSlice.reducer;
+export const { setFilter } = filterSlice.actions;
+export const getFilterValue = state => state.filter.value;
